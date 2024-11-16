321afe222f2642d40cc0c5479fc7201a127fdaef
OnlyPascal <chuyengianhorang@gmail.com>
2024-11-16 10:34:55 +0700

    refactor name, auto run script

--- clean_docker.sh
+++ clean_docker.sh
@@ -3,8 +3,8 @@
 echo '=> stopping containers ...'
 docker stop $(docker ps -a -q)
 
-echo '=> removing containers ...'
-docker rm $(docker ps -a -q)
+# echo '=> removing containers ...'
+# docker rm $(docker ps -a -q)
 
 # echo '=> deleteing images ..'
 # docker image rm $(docker images -q) 
--- run_docker.sh
+++ run_docker.sh
@@ -1,13 +1,78 @@
 #!/bin/bash
 
-docker_path=$1
-opts=$2
+# docker_path=$1
+# opts=$2
 
-docker compose \
-  -f $docker_path/docker-compose.yml \
-  --env-file .env \
-  up \
-  --detach \
-  --build \
-  $opts
+# docker compose \
+#   -f $docker_path/docker-compose.yml \
+#   --env-file .env \
+#   up \
+#   --detach \
+#   --build \
+#   $opts
 
+# Function to display usage
+usage() {
+  echo "Usage: $0 -auto | -name <name>"
+  exit 1
+}
+
+# Function for auto mode
+auto_mode() {
+  echo "Auto mode selected."
+
+  echo 'Stopping containers ...'
+  docker stop $(docker ps -a -q)
+  
+  local containers=("warehouse" \
+    "kafka" \
+    "producer-owm" \
+    "consumer-logging" \
+    "consumer-visualize" \
+    )
+  
+  echo 'Rerunning containers ...'
+  for container in "${containers[@]}"; do
+    echo "Running container: $container"
+    name_mode "$container"
+    sleep 10
+  done
+}
+
+# Function for name mode
+name_mode() {
+  local docker_path=$1
+  local opts=$2
+  if [ -z "$docker_path" ]; then
+      echo "Error: Missing name argument."
+      usage
+  fi
+
+  echo "Docker path: $docker_path || Opts: $opts"
+  docker compose \
+    -f $docker_path/docker-compose.yml \
+    --env-file .env \
+    up \
+    --detach \
+    --build \
+    $opts
+}
+
+# Check if no arguments are passed
+if [ $# -eq 0 ]; then
+  usage
+fi
+
+# Parse arguments
+case "$1" in
+  -auto)
+    auto_mode
+    ;;
+  -name)
+    name_mode "$2" "$3"
+    ;;
+  *)
+    echo "Error: Invalid option."
+    usage
+    ;;
+esac
