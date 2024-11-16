033473f2ef568158e9595387b8c729a4a4befbda
OnlyPascal <chuyengianhorang@gmail.com>
2024-11-16 10:01:42 +0700

    refactor folder name

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo '=> stopping containers ...'
 docker stop $(docker ps -a -q)
 
-# docker rm $(docker ps -a -q)
+echo '=> removing containers ...'
+docker rm $(docker ps -a -q)
 
+# echo '=> deleteing images ..'
 # docker image rm $(docker images -q) 
 
