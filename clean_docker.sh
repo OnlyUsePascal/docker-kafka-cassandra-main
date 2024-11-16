69b0a8c3b4f7cb62d4f59d2dc9adac4760eb4f20
OnlyPascal <chuyengianhorang@gmail.com>
2024-11-16 16:35:14 +0700

    clean docker script, make kafka-connect more stable: json config, image latest version

--- clean_docker.sh
+++ clean_docker.sh
@@ -3,8 +3,11 @@
 echo '=> stopping containers ...'
 docker stop $(docker ps -a -q)
 
-# echo '=> removing containers ...'
-# docker rm $(docker ps -a -q)
+if [ "$1" = "clean" ]; then
+  echo '=> removing containers ...'
+  docker rm $(docker ps -a -q)
+fi
+
 
 # echo '=> deleteing images ..'
 # docker image rm --force $(docker images -q) 
