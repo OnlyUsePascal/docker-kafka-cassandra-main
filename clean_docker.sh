3c84a6e1ebccd38d5be7d7520f4f286e6a97aac6
OnlyPascal <chuyengianhorang@gmail.com>
2024-11-22 17:09:10 +0700

    clean docker script - opt for removing image

--- clean_docker.sh
+++ clean_docker.sh
@@ -3,11 +3,15 @@
 echo '=> stopping containers ...'
 docker stop $(docker ps -a -q)
 
-if [ "$1" = "clean" ]; then
+if [ "$1" = "proc" ]; then
   echo '=> removing containers ...'
   docker rm $(docker ps -a -q)
 fi
 
+if [ "$2" = "img" ]; then
+  echo '=> deleting images ...'
+  docker image rm --force $(docker images -q) 
+fi
 
 # echo '=> deleteing images ..'
 # docker image rm --force $(docker images -q) 
